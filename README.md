528b3abe6ee21caa314a88f104aa0cd9b2d42569
Nawaz Danish <nawazdanish1996@gmail.com>
2022-07-12 01:03:49 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Javascript-Interview-Series
+Javascript Interview Series With Thapa Technica
+
+Javascript-Interview-Series Link: https://www.youtube.com/playlist?list=PLwGdqUZWnOp0VVqPVPA3ccqtjpLLPwtN8
--- removeDuplicatesFromArray/removeDuplicateFromArray.js
+++ removeDuplicatesFromArray/removeDuplicateFromArray.js
@@ -1,2 +1,11 @@
-let arr = [1, 1, 1, 2, 5, 5, 3, 9, 6];
+let arr = [1, 1, 1, 2, 5, 5, 3, 9, 6, 7, 3, 3, 4, 9, 4];
 console.log(arr);
+
+let arr1 = [ ...new Set(arr)];
+console.log(arr1);
+
+/*
+new set(arr) is a Set containing all the values in arr,
+which duplicates necessarily removed. Then the spread
+operator just convert this back into an array.
+*/
