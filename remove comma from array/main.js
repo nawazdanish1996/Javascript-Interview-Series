87294a66122a34ab1f9a979a10ce7b72f0916d83
Nawaz Danish <nawazdanish1996@gmail.com>
2022-07-12 15:36:03 +0530

    initial commit

---
+++ remove comma from array/main.js
@@ -0,0 +1,4 @@
+let arr = ['alpha', 'bravo', 'charlie'];
+console.log(arr);
+console.log(arr.join()); // remove comma from array
+console.log(arr);
---
+++ remove spaces from array/main.js
@@ -0,0 +1,4 @@
+let a = "    Mister    "
+console.log(a.length);
+a = a.trim();
+console.log(a.length);
