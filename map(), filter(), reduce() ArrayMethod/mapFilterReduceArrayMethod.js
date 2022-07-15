3099ffafaf8ef1a099345783d716123d62a1ea17
Nawaz Danish <nawazdanish1996@gmail.com>
2022-07-15 19:40:52 +0530

    add

--- map(), filter(), reduce() ArrayMethod/mapFilterReduceArrayMethod.js
+++ map(), filter(), reduce() ArrayMethod/mapFilterReduceArrayMethod.js
@@ -32,18 +32,25 @@
 // filter()
 // filter() function is basically used to filter the function of array.
 
-const arr = [5, 1, 3, 2, 6];
+// const arr = [5, 1, 3, 2, 6];
 // filter odd values inside the array.
 // function isOdd(x){
 //     return x % 2;
 // }
-function isEven(x){
-    return x % 2 === 0;
-}
+// function isEven(x){
+//     // return x % 2 === 0; // is Even value
+//     // return x % 2; // is odd
+//     return x > 4; // greater than four
+// }
 
-const output = arr.filter(isEven);
-console.log(output);
+// const output = arr.filter((x) => x < 3);
+// console.log(output);
 
 // reduce()
 /* The reduce() method does not execute the function for empty
 array elements. The reduce() method does not change the original array. */
+
+const arr = [5, 1, 3, 2, 6];
+
+const b = arr.indexOf(2);
+console.log(b);
